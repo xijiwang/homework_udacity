11cc21837bb64b25e464b3c666c4d4092de4c8ff
xijiwang <xjwang.coder@gmail.com>
2017-04-20 20:32:35 +0800

    fixed magic numbers and variable names.

--- my_fill_in_the_blanks.py
+++ my_fill_in_the_blanks.py
@@ -4,29 +4,31 @@ q_and_a = {
     'easy': {
         'question': '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,tuple, and ___4___ or can be more complicated such as objects and lambda functions. ''',
         'answer': [
-            ['1', ["function", "Function"], 'F'],
-            ['2', ["argument", "Argument"], 'A'],
-            ['3', ["none", "None"], 'N'],
-            ['4', ["list", "List"], 'L']
+            {'question_id': '1', 'list_answer': ["function", "Function"], 'first_letter': 'F'},
+            {'question_id': '2', 'list_answer': ["argument", "Argument"], 'first_letter': 'A'},
+            {'question_id': '3', 'list_answer': ["none", "None"], 'first_letter': 'N'},
+            {'question_id': '4', 'list_answer': ["list", "List"], 'first_letter': 'L'}
         ]
     },
     'medium': {
         'question': '''A ___1___ program is usually written by a ___1___ programmer in a programming language. From the program in its human-readable form of source code, a ___2___ can derive machine code : a form consisting of ___3___ that the ___1___ can directly execute. Alternatively, a ___1___ program may be executed with the aid of an ___4___. ''',
         'answer': [
-            ['1', ["computer", "Computer"], 'C'],
-            ['2', ["compiler", "Compiler"], 'C'],
-            ['3', ["instructions", "Instructions"], 'I'],
-            ['4', ["interpreter", "Interpreter"], 'I']]
+            {'question_id': '1', 'list_answer': ["computer", "Computer"], 'first_letter': 'C'},
+            {'question_id': '2', 'list_answer': ["compiler", "Compiler"], 'first_letter': 'C'},
+            {'question_id': '3', 'list_answer': ["instructions", "Instructions"], 'first_letter': 'I'},
+            {'question_id': '4', 'list_answer': ["interpreter", "Interpreter"], 'first_letter': 'I'}
+        ]
     },
     'hard': {
         'question': '''___1___ are small, chubby, and incredibly cute mouse-like Pokémon. They are almost completely covered by ___2___ fur. They have long ___2___ ears that are tipped with black. A ___1___'s back has two brown stripes, and its large tail is notable for being shaped like a lightning bolt. On its ___3___ are two circle-shaped red pouches used for storing ___4___. They turn ___2___ and spark with ___4___ when its about to use an Electric attack, such as Thunderbolt. It has also been known to ___5___ small surges of electrical energy in anger or for protection, like in the anime. ''',
         'answer': [
-            ['1', ["pikachu", "Pikachu"], 'P'],
-            ['2', ["yellow", "Yellow"], 'Y'],
-            ['3', ["cheeks", "Cheeks"], 'C'],
-            ['4', ["electricity", "Electricity"], 'E'],
-            ['5', ["generate", "Generate"], 'G']
+            {'question_id': '1', 'list_answer': ["pikachu", "Pikachu"], 'first_letter': 'P'},
+            {'question_id': '2', 'list_answer': ["yellow", "Yellow"], 'first_letter': 'Y'},
+            {'question_id': '3', 'list_answer': ["cheeks", "Cheeks"], 'first_letter': 'C'},
+            {'question_id': '4', 'list_answer': ["electricity", "Electricity"], 'first_letter': 'E'},
+            {'question_id': '5', 'list_answer': ["generate", "Generate"], 'first_letter': 'G'}
         ]
+
     }
 }
 
@@ -36,11 +38,11 @@ def check_answer(question, q_id, list_ans, first_letter):
     answer = raw_input("Which statement should it put in ___" + q_id + "___? ")
     if answer == list_ans[0] or answer == list_ans[1]:
         print "answer is correct. ___" + q_id + "___ is " + list_ans[0] + "."
-        idx = 0
+        index = 0
 
         for whatever in question:
-            question[idx] = question[idx].replace("___" + q_id + "___", list_ans[0])
-            idx += 1
+            question[index] = question[index].replace("___" + q_id + "___", list_ans[0])
+            index += 1
 
         print " ".join(question)
         print ""
@@ -60,8 +62,8 @@ def game(level):
     print question
     question = question.split(" ")
 
-    for i in range(len(list_answer)):
-        while not check_answer(question, list_answer[i][0], list_answer[i][1], list_answer[i][2]):
+    for answer in range(len(list_answer)):
+        while not check_answer(question, list_answer[answer]['question_id'], list_answer[answer]['list_answer'], list_answer[answer]['first_letter']):
             print ""
 
     print "Final answer:"
